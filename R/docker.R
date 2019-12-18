488f11886f9289057fcc03f4dc59c7da7090bbe6
Aaron Peikert <aaron.peikert@posteo.de>
2019-12-18 11:27:44 +0100

    export use_docker

--- R/docker.R
+++ R/docker.R
@@ -5,6 +5,7 @@
 #' @param rver which r version to use, defaults to current version.
 #' @param stack which stack to use, possible values are `c("r-ver", "rstudio", "tidyverse", "verse", "geospatial")`.
 #' @param date which date should be used for package instalation, defaults to today.
+#' @export
 
 use_docker <- function(rver = NULL, stack = "verse", date = Sys.Date()){
   if (is.null(rver)) {
