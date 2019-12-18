2f1cf824c9b006031c877fc489be41b49f763bb2
Aaron Peikert <aaron.peikert@posteo.de>
2019-12-18 11:29:22 +0100

    include docker in make

--- R/make.R
+++ R/make.R
@@ -1,24 +1,66 @@
 #' Use Make
 #'
-#' Add or modify the Makefile in the current project.
+#' Add a (GNU-)Makefile(s) with special emphasis on the use of containers.
+#' @param docker If true a setup is created that can partially send make commands to a Docker container
+#' @param singularity If true a setup is created that can partially send make commands to a Singularity container (which requires the Dockerimage)
+#' @param torque If true a setup is created that can partially send make comands to a TORQUE job scheduler. Especially usefull in combination with a Singularity container.
+#' @param use_docker If true `use_docker()` is called.
+#' @param dockerignore If true a .dockerignore file is created.
+#' @name make
+NULL
+
+#' @rdname make
 #' @export
-use_make <- function(){
-  # read dockerfile
-  Makefile <- usethis::proj_path("Makefile")
-  if(fs::file_exists(Makefile)){
+use_make <- function(docker = FALSE, singularity = FALSE, torque = FALSE){
+  if(docker)use_make_docker()
+  template_data <- list(wrapper = FALSE,
+                        docker = FALSE,
+                        winpath = NULL,
+                        singularity = FALSE,
+                        torque = FALSE)
+  if(fs::file_exists("Makefile_Docker") & docker){
+    template_data$wrapper <- TRUE
+    template_data$docker <- TRUE
+    template_data$winpath <- docker_windows_path(
+      "C:/Users/someuser/Documents/myproject/"
+      )
+  }
+  if(fs::file_exists("Makefile")){
     usethis::ui_oops("Makefile already exists.")
   } else {
+    usethis::use_template(
+      "Makefile.txt",
+      "Makefile",
+      data = template_data,
+      ignore = TRUE,
+      open = TRUE,
+      package = "repro"
+    )
     rmds <- fs::path_rel(
       fs::dir_ls(usethis::proj_path(), glob = "*.Rmd", recurse = TRUE),
       usethis::proj_path()
     )
     usethis::ui_info("You probably want to add:\n{usethis::ui_value(rmds)}\nto the {usethis::ui_value('Makefile')}.\nHint: {usethis::ui_code('repro::use_make_rmd()')}")
-    save_as <- "Makefile"
+  }
+}
+
+#' @rdname make
+#' @export
+use_make_docker <- function(use_docker = TRUE, dockerignore = TRUE){
+  if(!fs::file_exists("Dockerfile") & use_docker)use_docker()
+  usethis::use_template(
+    "Makefile_Docker",
+    "Makefile_Docker",
+    ignore = TRUE,
+    open = FALSE,
+    package = "repro"
+  )
+  if(dockerignore){
     usethis::use_template(
-      "Makefile.txt",
-      save_as,
+      "dockerignore",
+      ".dockerignore",
       ignore = TRUE,
-      open = TRUE,
+      open = FALSE,
       package = "repro"
     )
   }
