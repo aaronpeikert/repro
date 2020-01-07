833caefa8ef89803b5da747a2e37acc40b3052e5
Aaron Peikert <aaron.peikert@posteo.de>
2020-01-07 12:02:52 +0100

    add an option to fake os

--- R/zzz.R
+++ R/zzz.R
@@ -10,7 +10,8 @@
     repro.make = NA,
     repro.git = NA,
     repro.choco = NA,
-    repro.brew = NA
+    repro.brew = NA,
+    repro.os = NA
     )
   toset <- !(names(op.repro) %in% names(op))
   if(any(toset)) options(op.repro[toset])
@@ -20,18 +21,24 @@
 
 get_os <- function(){
   sysinf <- Sys.info()
-  if (!is.null(sysinf)){
-    os <- sysinf['sysname']
-    if (os == 'Darwin')
-      os <- "osx"
-  } else { ## mystery machine
-    os <- .Platform$OS.type
-    if (grepl("^darwin", R.version$os))
-      os <- "osx"
-    if (grepl("linux-gnu", R.version$os))
-      os <- "linux"
+  if(is.na(getOption("repro.os"))){
+    if (!is.null(sysinf)){
+      os <- sysinf['sysname']
+      if (os == 'Darwin')
+        os <- "osx"
+    } else { ## mystery machine
+      os <- .Platform$OS.type
+      if (grepl("^darwin", R.version$os))
+        os <- "osx"
+      if (grepl("linux-gnu", R.version$os))
+        os <- "linux"
+    }
+    os <- tolower(os)
+    options(repro.docker = os)
+    return(os)
+  } else {
+    return(getOption("repro.os"))
   }
-  tolower(os)
 }
 
 silent_command <- function(...){
