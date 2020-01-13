b03a28a221e73d8588567a4fb943e83e11cb8bce
Aaron Peikert <aaron.peikert@posteo.de>
2020-01-13 13:08:09 +0100

    fix regression here too r-lib/usethis#928

--- R/yaml.R
+++ R/yaml.R
@@ -2,6 +2,13 @@ read_yaml <- function(path, ...){
   x <- readLines(path)
   yaml_ind <- which(x == "---")
   stopifnot(length(yaml_ind) == 2L)
-  yaml <- yaml::read_yaml(text = x[seq(yaml_ind[[1]], yaml_ind[[2]])])
+  stripped <- stringr::str_c(x[seq(yaml_ind[[1]] + 1, yaml_ind[[2]] - 1)],
+                      collapse = "\n")
+  yaml <- yaml::read_yaml(text = stripped, ...)
   return(yaml)
+}
+repro_yaml <- function(path, ...){
+  yaml <- read_yaml(path, ...)
+  if(!("repro" %in% names(yaml)))stop("This is not a repro-yaml.")
+  yaml$repro
 }
--- tests/testthat/test-yaml.R
+++ tests/testthat/test-yaml.R
@@ -1,7 +1,5 @@
 context("Test yaml related functions")
-test_that("yaml is corectly read", {
-  scoped_temporary_project()
-  cat('
+test_file1 <- '
 ---
 title: "Test"
 author: "Aaron Peikert"
@@ -26,7 +24,22 @@ knitr::opts_chunk$set(echo = TRUE)
 ## R Markdown
 
 This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
-', file = "test.Rmd")
-yaml <- read_yaml("test.Rmd")
-expect_identical(names(yaml), c("title", "author", "date", "output", "repro"))
+
+'
+
+test_that("yaml is corectly read", {
+  scoped_temporary_project()
+  cat(test_file1, file = "test.Rmd")
+  expect_identical(names(read_yaml("test.Rmd")),
+                   c("title", "author", "date", "output", "repro"))
+  expect_identical(names(repro_yaml("test.Rmd")),
+                   c("packages", "data", "scripts"))
+})
+
+options()
+test_that("informs is weird.", {
+  withr::with_options(c(crayon.enabled = FALSE), {
+    expect_message(rlang::inform("test"), "test")
+    })
+  expect_message(rlang::inform("test"), "test")
 })
