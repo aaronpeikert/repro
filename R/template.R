ff43af69a1b517f5a80c98739b5c0d385dc72980
Aaron Peikert <aaron.peikert@posteo.de>
2020-01-27 15:39:45 +0100

    add template

---
+++ R/template.R
@@ -0,0 +1,14 @@
+repro_template <- function(path, ...) {
+  # ensure path exists
+  fs::dir_create(path, recurse = TRUE)
+  usethis::proj_set(path, force = TRUE)
+  usethis::use_template("markdown.Rmd",
+                        data = list(date = Sys.Date(), ...),
+                        package = "repro")
+  fs::dir_create(usethis::proj_path("data"))
+  fs::dir_create(usethis::proj_path("R"))
+  write.csv(mtcars, usethis::proj_path("data", "mtcars.csv"))
+  usethis::use_template("clean.R",
+                        "R/clean.R",
+                        package = "repro")
+}
