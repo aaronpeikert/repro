eb4522a074de60716bf005b12af43fc38d0834e8
Aaron Peikert <aaron.peikert@posteo.de>
2020-01-27 10:02:25 +0100

    add docker bundle

--- tests/testthat/test-automate.R
+++ tests/testthat/test-automate.R
@@ -9,6 +9,25 @@ test_that("docker automate works", {
   expect_proj_file(".repro", "Dockerfile_base")
   expect_proj_file(".repro", "Dockerfile_packages")
   expect_proj_file(".repro", "Dockerfile_manual")
+  expect_proj_file("Dockerfile")
+})
+
+test_that("automate respects options", {
+  opts <- options()
+  scoped_temporary_project()
+  cat(test_rmd1, file = "test.Rmd")
+  fs::dir_create("test")
+  cat(test_rmd2, file = "test/test2.Rmd")
+  options(repro.dir = "somedir",
+          repro.dockerfile.base = "base",
+          repro.dockerfile.manual = "manual",
+          repro.dockerfile.packages = "packages")
+  automate_docker()
+  expect_proj_dir("somedir")
+  expect_proj_file("somedir", "base")
+  expect_proj_file("somedir", "packages")
+  expect_proj_file("somedir", "manual")
+  options(opts)
 })
 
 test_that("automate dir works", {
