7c3f3387ac94176dccee9db02aa68f9033347a00
Aaron Peikert <aaron.peikert@posteo.de>
2021-07-15 10:49:55 +0200

    actually passing of args to reproduce

--- R/rerun.R
+++ R/rerun.R
@@ -2,17 +2,12 @@
 #'
 #' `rerun()` was renamed to [`reproduce()`]. `rerun()` is deprecated.
 #'
-#' @param fun a function that inspects `dir` and advises on how to reproduce the analysis. Defaults to [reproduce_funs].
-#' @param ... more functions like `fun`.
-#' @param path Were should I look for entrypoints?
-#' @param cache Default is `FALSE`. Some entrypoints have a cache, which you probably do not want to use in a reproduction.
-#' @param silent Should a message be presented?
-#' @return Returns invisibly `TRUE` if an entrypoint was found and `FALSE` if not.
+#' @param ... passed to [`reproduce()`].
 #' @seealso reproduce_funs
 #' @name rerun
 #' @export
 
-rerun <- function(fun, ..., path = ".", cache = FALSE, silent = FALSE){
+rerun <- function(...){
   usethis::ui_warn("{usethis::ui_code('rerun()')} is deprecated/renamed.\nUse {usethis::ui_code('reproduce()')} instead.")
-  reproduce(fun, ..., path = ".", cache = FALSE, silent = FALSE)
+  reproduce(...)
 }
